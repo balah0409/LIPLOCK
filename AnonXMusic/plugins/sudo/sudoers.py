85dc2af75bc820ccd66c799dab36ef3e59c270b5
hyperaj <168776874+hyperaj@users.noreply.github.com>
2024-05-05 18:08:42 +0530

    Update sudoers.py

--- AnonXMusic/plugins/sudo/sudoers.py
+++ AnonXMusic/plugins/sudo/sudoers.py
@@ -44,7 +44,7 @@ async def userdel(client, message: Message, _):
         await message.reply_text(_["sudo_8"])
 
 
-@app.on_message(filters.command(["hyperlist", "sudoers"]) & ~BANNED_USERS)
+@app.on_message(filters.command(["hyperlist"]) & ~BANNED_USERS)
 @language
 async def sudoers_list(client, message: Message, _):
     text = _["sudo_5"]
