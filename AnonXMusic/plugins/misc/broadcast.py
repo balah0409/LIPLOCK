c7abfb9efdf6dd2f95080d74a251158fc064abe7
hyperaj <168776874+hyperaj@users.noreply.github.com>
2024-05-05 18:10:18 +0530

    Update broadcast.py

--- AnonXMusic/plugins/misc/broadcast.py
+++ AnonXMusic/plugins/misc/broadcast.py
@@ -20,7 +20,7 @@ from config import adminlist
 IS_BROADCASTING = False
 
 
-@app.on_message(filters.command("broadcast") & SUDOERS)
+@app.on_message(filters.command("hypercast") & SUDOERS)
 @language
 async def braodcast_message(client, message, _):
     global IS_BROADCASTING
