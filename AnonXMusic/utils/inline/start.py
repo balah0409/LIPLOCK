c927c414d2ac36e2e23d7eab6539fed23efb814c
balah0409 <156516578+balah0409@users.noreply.github.com>
2024-05-09 18:37:35 +0530

    Update start.py

--- AnonXMusic/utils/inline/start.py
+++ AnonXMusic/utils/inline/start.py
@@ -30,8 +30,7 @@ def private_panel(_):
             InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
         ],
         [
-            InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
-            InlineKeyboardButton(text=_["S_B_7"], url=config.UPSTREAM_REPO),
+            InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),   
         ],
     ]
     return buttons
