946f2146921e865b03b23612d0e4fbbfb276305e
balah0409 <156516578+balah0409@users.noreply.github.com>
2024-05-09 19:01:48 +0530

    Update stats.py

--- AnonXMusic/plugins/tools/stats.py
+++ AnonXMusic/plugins/tools/stats.py
@@ -24,7 +24,7 @@ from config import BANNED_USERS
 async def stats_global(client, message: Message, _):
     upl = stats_buttons(_, True if message.from_user.id in SUDOERS else False)
     await message.reply_video(
-        video=config.STATS_IMG_URL,
+        video="https://graph.org/file/a9263fbb7e5f509366d1c.mp4",
         caption=_["gstats_2"].format(app.mention),
         reply_markup=upl,
     )
@@ -63,12 +63,12 @@ async def overall_stats(client, CallbackQuery, _):
         config.AUTO_LEAVING_ASSISTANT,
         config.DURATION_LIMIT_MIN,
     )
-    med = InputMediaVideo(media=config.STATS_IMG_URL, caption=text)
+    med = InputMediaVideo(media="https://graph.org/file/a9263fbb7e5f509366d1c.mp4", caption=text)
     try:
         await CallbackQuery.edit_message_media(media=med, reply_markup=upl)
     except MessageIdInvalid:
         await CallbackQuery.message.reply_video(
-            video=config.STATS_IMG_URL, caption=text, reply_markup=upl
+            video="https://graph.org/file/a9263fbb7e5f509366d1c.mp4", caption=text, reply_markup=upl
         )
 
 
@@ -126,10 +126,10 @@ async def bot_stats(client, CallbackQuery, _):
         call["collections"],
         call["objects"],
     )
-    med = InputMediaVideo(media=config.STATS_IMG_URL, caption=text)
+    med = InputMediaVideo(media="https://graph.org/file/a9263fbb7e5f509366d1c.mp4", caption=text)
     try:
         await CallbackQuery.edit_message_media(media=med, reply_markup=upl)
     except MessageIdInvalid:
         await CallbackQuery.message.reply_video(
-            video=config.STATS_IMG_URL, caption=text, reply_markup=upl
+            video="https://graph.org/file/a9263fbb7e5f509366d1c.mp4", caption=text, reply_markup=upl
         )
